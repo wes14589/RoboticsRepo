dd6a1b647ec583d8bbcf61674c6d10d823dd121e
wes14589 <36731762+wes14589@users.noreply.github.com>
2019-02-08 09:39:54 +0000

    Add files via upload

---
+++ command_velocity.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  1 10:33:35 2019
+
+@author: student
+"""
+
+
+import rospy
+from geometry_msgs.msg import Twist # This is the message type the robot uses for velocities
+
+
+class CommandVelocity():
+    """Driving my robot
+    """
+
+    def __init__(self):
+        rospy.loginfo("Starting node")
+        self.pub = rospy.Publisher("/mobile_base/commands/velocity", Twist) # Creating a publisher with whatever name...
+        
+    # A function to send velocities until the node is killed
+    def send_velocities(self):
+        r = rospy.Rate(10) # Setting a rate (hz) at which to publish
+        while not rospy.is_shutdown(): # Runnin until killed
+            rospy.loginfo("Sending commands")
+            twist_msg = Twist() # Creating a new message to send to the robot
+
+            # ... put something relevant into your message
+            twist_msg.linear.x = 1
+            twist_msg.linear.y = 0
+            twist_msg.linear.z = 0
+            twist_msg.angular.x = 0
+            twist_msg.angular.y = 0
+            twist_msg.angular.z = 0.5
+            
+                
+
+            self.pub.publish(twist_msg) # Sending the message via our publisher
+            r.sleep() # Calling sleep to ensure the rate we set above
+
+if __name__ == '__main__':
+    rospy.init_node("command_velocity")
+    cv = CommandVelocity()
+    cv.send_velocities() # Calling the function
+    rospy.spin()
